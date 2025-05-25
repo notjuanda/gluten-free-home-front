a6ca7f136846bd3b53f7be595111a782b4d23c4c
Juan David Viveros <743711@nur.edu.bo>
2025-05-25 19:10:42 -0400

    chore: setup base con Tailwind, router y estructura de módulos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@modules": path.resolve(__dirname, "src/modules"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@providers": path.resolve(__dirname, "src/providers")
+    }
+  }
+});
