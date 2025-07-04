21ec9c7f9d84635072a0cc06f06a565afe62b0cf
Juan David Viveros <743711@nur.edu.bo>
2025-07-03 21:48:42 -0400

    fix: productos muestra 4 por fila

--- src/modules/clients/components/ProductsList.tsx
+++ src/modules/clients/components/ProductsList.tsx
@@ -4,7 +4,7 @@ import type { ProductsListProps } from '../types/products-components.type';
 
 const ProductsList: React.FC<ProductsListProps> = ({ products, onAddToCart }) => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 sm:gap-6 lg:gap-8">
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
       {products.map((product: Product) => (
         <ProductCard key={product.id} product={product} onAddToCart={onAddToCart} />
       ))}
